04d4d95be57a9e521e94e941840254b8273c5106
GreenMask <piotr88m@gmail.com>
2013-02-12 12:26:18 +0100

    remove jpg

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,8 @@ class Photo < ActiveRecord::Base
   belongs_to :user
   belongs_to :location
   
+  skip_callback :destroy, :after, :remove_image!
+  
   attr_accessible :description, :image, :name, :location_id
   mount_uploader :image, ImageUploader
 
